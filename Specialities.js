14dda0f11380adc3f2ca6b1301a43658b6491cb4
Noor AlAfeshat <48603376+n-ala@users.noreply.github.com>
2025-01-29 13:15:56 +0300

    Update Specialities.js

--- Specialities.js
+++ Specialities.js
@@ -42,7 +42,7 @@ const server = http.createServer((req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
